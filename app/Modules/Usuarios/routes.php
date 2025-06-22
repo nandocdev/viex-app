38cc2fb1b8236ef8aa5b976fceaf7f79f07b7cf8
Fernando Castillo <ferncastillov@outlook.com>
2025-06-22 14:42:40 -0500

    create modules

--- app/Modules/Usuarios/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Phast\System\Routing\Facades\Router;
-
-Router::group(['prefix' => 'usuarios', 'middleware' => []], function() {
-   Router::get('/', 'Usuarios\Controllers\UsuariosController@indexAction')->name('usuarios.index');
-});
